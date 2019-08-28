76bbce802aa1abdf4b545f1ee7503676b0e827e6
William Chow <wchow@William-Chow-MacBook-Pro.local>
2019-08-28 08:52:55 -0400

    first commit

---
+++ scripts/teardown.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+##
+# Script to remove/undepoy all project resources from k8s
+##
+
+# Delete mongos stateful set + mongod stateful set + mongodb service + secrets + host vm configurer daemonset
+kubectl delete statefulsets mongos-router
+kubectl delete services mongos-router-service
+kubectl delete statefulsets mongod-shard1
+kubectl delete services mongodb-shard1-service
+kubectl delete statefulsets mongod-shard2
+kubectl delete services mongodb-shard2-service
+kubectl delete statefulsets mongod-shard3
+kubectl delete services mongodb-shard3-service
+kubectl delete statefulsets mongod-configdb
+kubectl delete services mongodb-configdb-service
+kubectl delete secret shared-bootstrap-data
+sleep 3
+
+# Delete persistent volume claims
+kubectl delete persistentvolumeclaims -l tier=maindb
+kubectl delete persistentvolumeclaims -l tier=configdb
+sleep 3
+
+
