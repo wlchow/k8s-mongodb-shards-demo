e3b6da780c31b95235cc8f85cbaff4fdb8cf7716
William Chow <wchow@William-Chow-MacBook-Pro.local>
2019-09-26 16:23:27 -0400

    latest

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -4,6 +4,7 @@
 ##
 
 # Delete mongos stateful set + mongod stateful set + mongodb service + secrets + host vm configurer daemonset
+kubectl delete statefulsets mongosqld-bi
 kubectl delete statefulsets mongos-router
 kubectl delete services mongos-router-service
 kubectl delete statefulsets mongod-shard1
@@ -14,7 +15,10 @@ kubectl delete statefulsets mongod-shard3
 kubectl delete services mongodb-shard3-service
 kubectl delete statefulsets mongod-configdb
 kubectl delete services mongodb-configdb-service
+kubectl delete services mongosqld-bi-service
+kubectl delete services mongosqld-bi-service-external
 kubectl delete secret shared-bootstrap-data
+kubectl delete secret mongodb-keyfile
 sleep 3
 
 # Delete persistent volume claims
